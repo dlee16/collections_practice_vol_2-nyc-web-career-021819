8d444c6661b4051b4355ed29ba8d1402766e85b0
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-05 21:14:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -26,7 +26,7 @@ def merge_data(keys,data)
 end
 
 def find_cool(array)
-  array.select {|word| word if word.has_value?("cool")}
+  array.select {|word| if word.has_value?("cool")}
 end
 
 def organize_schools
